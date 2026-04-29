c1b716e9ff9a12d5c5015fd7179795fd4aec06b1
rangeset <2432647223@qq.com>
2026-04-29 21:50:26 +0800

    feat(boot): add file header

---
+++ boot/boot.asm
@@ -0,0 +1,10 @@
+; Copyright (c) 2026 Phiarc Team and St Rangeset
+; Licensed under the GNU General Public License v3.0
+; https://github.com/phi-amateur-radio-community/io-puzzle
+
+BITS 16
+ORG 0x7C00
+
+GLOBAL _start
+
+SECTION .text
