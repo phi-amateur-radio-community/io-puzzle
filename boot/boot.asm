44c577be682966b7df94dce7c4f5fbe061020833
rangeset <2432647223@qq.com>
2026-04-30 11:55:50 +0800

    feat(boot): add partition table

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,3 +8,39 @@ ORG 0x7C00
 GLOBAL _start
 
 SECTION .text
+start:
+
+; TODO: Boot Loader.
+
+time 446 - ($ - $$) db 0 ; Fill in the empty space
+
+
+; =========================================
+; Partition table
+; =========================================
+
+SECTION .mbr
+partition_table:
+
+; Entry 0 (1 ~ 2047):
+db 0x00             ; Boot flag
+db 0x00, 0x02, 0x00 ; Start CHS
+db 0xDA             ; Type: 0xDA (Customize)
+db 0xFE, 0xFF, 0xFF ; End CHS
+dd 0x00000001       ; Start sector (1)
+dd 0x000008FF       ; Sector count (2047)
+
+; Entry 1 (2048 ~ Unknown):
+db 0x00             ; Boot flag
+db 0x00, 0x02, 0x00 ; Start CHS
+db 0x0C             ; Type: 0x0C (FAT32 LBA)
+db 0xFE, 0xFF, 0xFF ; End CHS
+dd 0x00000800       ; Start sector (2048)
+dd 0x00000000       ; Sector count (Patch by core)
+
+; Entry 2 ~ 3 (Null):
+time 32 db 0 ; Fill in the empty space
+
+
+SECTION .boot_sig
+dw 0xAA55 ; MBR Boot Signature
