03190022079ca228a0afe5d54adf4a4a532c5e72
rangeset <2432647223@qq.com>
2026-05-01 10:33:36 +0800

    chore(boot): optimize the directory structure

--- boot/tools/build.sh
+++ boot/tools/build.sh
@@ -6,6 +6,6 @@ if [ ! -f "disk.img" ]; then
   bximage -q -hd="${IMG_SIZE}" -func=create -sectsize=512 -imgmode=flat disk.img
 fi
 
-nasm -f bin "${CURRENT_DIR}/boot.asm" -o boot.bin -I "${CURRENT_DIR}/include/"
+nasm -f bin "${CURRENT_DIR}/src/boot.asm" -o boot.bin -I "${CURRENT_DIR}/include/"
 
 dd if=boot.bin of=disk.img bs=512 conv=notrunc
